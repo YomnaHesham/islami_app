1a0e084a2d19839212ad8866188c554af96c9f97
Yomna Hesham <yomnaheshammohamed@gmail.com>
2024-07-31 13:13:56 +0300

    app init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main(){
+  runApp(IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routName: (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.routName,
+    );
+  }
+}
